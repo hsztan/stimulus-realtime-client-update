12fd4d633a9d78c7de2ff249b73ff08ab02aec02
Henry Nawrocki Sztanski <henry@sztanski.com>
2023-12-15 08:40:44 -0500

    Add start process functionality and update view

--- app/controllers/process_controller.rb
+++ app/controllers/process_controller.rb
@@ -1,5 +1,10 @@
 class ProcessController < ApplicationController
-  def index
+  def start
     ProgressIncrementJob.perform_later
+
+    respond_to do |format|
+      format.turbo_stream
+      format.js # Expects a file named start.js.erb
+    end
   end
 end
